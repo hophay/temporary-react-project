09859cef7d69a170f816e7e984faee4829ad3513
Hayden Hopkin <hopkinhayden@gmail.com>
2021-10-27 19:41:25 -0400

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Umbrellas Restaurant and Bar was originally conceptualized to be a casual beach bar, with delicious and easy food, amazing cocktails created with Westerhall Rums - all with the goal of an atmosphere that spreads good times and vibes. Years before Umbrellas existed, one of the owner's mother had a small bar named "The Umbrella Bar" which operated at the same location. This inspired the name, and expanded the story and evolution of what is now today Umbrellas.
+
+Umbrellas Restaurant and Bar was created as a form of the Westerhall Rums experience. The brand wanted a location that it could share its products  in an exciting way - what other way than a beach side restaurant and bar emulating the epitome of Caribbean culture by way of the beach, good rum, and good vibes.
+      </p>
+    </section>
+  )
+}
+
+export default About
